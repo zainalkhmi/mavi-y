cfd59cebc4d0af1cd05c816eeadf86cd00dd1243
Zainal Khmi <zainalkhmi@gmail.com>
2026-03-07 05:29:19 +0700

    mobile

--- mavi-mobile-reader/src/main.jsx
+++ mavi-mobile-reader/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
@@ -26,8 +26,8 @@ if ('serviceWorker' in navigator) {
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
